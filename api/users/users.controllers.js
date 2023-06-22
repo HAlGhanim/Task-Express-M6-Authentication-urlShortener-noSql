186851f925d16a3427e6efdc00fac058ff72f5e9
HAlGhanim <imzaji98@gmail.com>
2023-06-22 18:08:57 +0300

    sign up and generate token done

--- api/users/users.controllers.js
+++ api/users/users.controllers.js
@@ -1,26 +1,49 @@
-const User = require('../../models/User');
+const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const passHash = async (password) => {
+  const rounds = 10;
+  const hashedPassword = await bcrypt.hash(password, rounds);
+  return hashedPassword;
+};
+
+const generateToken = (user) => {
+  const payload = {
+    _id: user._id,
+    username: user.username,
+  };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_TOKEN_EXP,
+  });
+  return token;
+};
 
 exports.signin = async (req, res) => {
   try {
   } catch (err) {
-    res.status(500).json('Server Error');
+    res.status(500).json("Server Error");
   }
 };
 
 exports.signup = async (req, res) => {
   try {
+    const { password } = req.body;
+    req.body.password = await passHash(password);
     const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
+    const token = generateToken(newUser);
+    res.status(201).json(token);
   } catch (err) {
-    res.status(500).json('Server Error');
+    res.status(500).json("Server Error");
   }
 };
 
 exports.getUsers = async (req, res) => {
   try {
-    const users = await User.find().populate('urls');
+    const users = await User.find().populate("urls");
     res.status(201).json(users);
   } catch (err) {
-    res.status(500).json('Server Error');
+    res.status(500).json("Server Error");
   }
 };
